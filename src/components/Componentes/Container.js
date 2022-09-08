e7a758dafca1028a9e1b05926a3a8a6411cc89fb
MMolina <marcosgmolina998@gmail.com>
2022-09-08 19:43:35 -0300

    Objetos carrito

--- src/components/Componentes/Container.js
+++ src/components/Componentes/Container.js
@@ -1,9 +1,27 @@
 import ItemCount from "./ItemCount"
+import data from "./mockData";
+import ItemList from "./ItemList";
+import { useEffect, useState } from "react";
+
 const ItemListContainer = ({greeting}) => {
+  const [productList, setProductList] = useState([]);
+
+    useEffect(()=> {
+      getItems.then((response) =>{
+        setProductList(response);
+      })
+      .catch(error => console.log(error));
+    }, []);
   const stock=10;
+
+  const getItems = new Promise((resolve,reject)=> {
+    setTimeout(()=>{
+      resolve(data);
+    }, 2000)
+  })
   return (
     <div>
-      <h1 className="saludo">{greeting}</h1>
+      <ItemList lista= {productList} />
      
      <ItemCount stock={stock} />
     </div>
---
+++ src/components/Componentes/mockData.js
@@ -0,0 +1,35 @@
+
+const data = [
+    {
+        id: "01",
+        title: "Pantalon adidas",
+        price: 3000,
+        Image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/adidas-pantalon-chandal-1576501573.png?crop=0.997624703087886xw:1xh;center,top&resize=480:*',
+    },
+    {
+        id: "02",
+        title: "Short deportivo",
+        price: 1000,
+        Image: 'https://www.muskblog.com/wp-content/uploads/2019/06/SHORT-F50-Holand-Negro-Blanco-Lateral-560x560.png',
+    },
+    {
+        id: "03",
+        title: "Remera deportiva mujer",
+        price: 2000,
+        Image: 'https://http2.mlstatic.com/D_NQ_NP_762111-MLA40003691898_122019-W.jpg',
+    },
+    {
+        id: "04",
+        title: "Pantalon largo deportivo",
+        price: 2500,
+        Image: 'https://cf.shopee.com.ar/file/53cb4117245af0ecdf8d3c3c718fe348_tn',
+    },
+    {
+        id: "05",
+        title: "Conjunto deportivo",
+        price: 3500,
+        Image: 'https://cf.shopee.com.mx/file/977bb3b942d8cc211c0bfd3a1098c2c3_tn',
+    }
+]
+
+export default data;
